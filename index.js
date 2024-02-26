4a49f35c2e288e03a2fbfbd56679d8d7fdf6c024
CRBox <dharianfenix@gmail.com>
2024-02-26 12:35:14 +0100

    Primera Subida

---
+++ index.js
@@ -0,0 +1,21 @@
+function suma (x,y){
+        return x + y;
+}
+
+function subs (x,y){
+    return x - y;
+}
+
+function mult (x,y){
+    return x * y;
+}
+
+function div (x,y){
+    return x / y;
+}
+
+function cuadrado (x){
+    return x **2;
+}
+
+module.exports ={suma, subs, mult, div}
